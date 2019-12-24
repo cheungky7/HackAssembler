60a152c7d73ca372d28b57ca1120593fe0007f51
cheungky <kycheung1985@gmail.com>
2019-12-24 23:32:53 +0800

    Use bitset instead

--- binarycoder.cpp
+++ binarycoder.cpp
@@ -15,28 +15,12 @@ binarycoder *binarycoder::getInstance(){
       return instance;
 }
 
+
 std::string binarycoder::SymbolToAInstr(std::string symbol){
     size_t sz;
     int number= std::stoi(symbol,&sz,10);
-    char temp2[16]={0};
-    char temp[16]={0};
-    memset (temp2,'0',16);
-    memset (temp,'0',16);
-    int digit=0;
-    int init=number;
-    for(digit=0; number>0; digit++){
-        temp[digit]=(char)number%2+'0';
-        number= number/2;
-    }
-    if(init%2==0){
-        digit=digit+1;
-    }
-
-
-
-    memcpy ( (char*)&temp2[16-digit],(char*) temp, digit );
-    std::string str(temp2);// string constructor deep copy
-    return str;
+    std::bitset<16> symbolbits(number);
+    return symbolbits.to_string();
 
 }
 
